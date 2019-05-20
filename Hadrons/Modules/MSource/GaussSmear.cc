76538b565c0dd1a6e1dd905c52bba35418777c47
Nils Asmussen <n.asmussen@soton.ac.uk>
2019-05-20 17:25:19 +0100

    add Modules MSource::GaussSmear and MSource::JacobiSmear

---
+++ Hadrons/Modules/MSource/GaussSmear.cc
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MSource/GaussSmear.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MSource;
+
+template class Grid::Hadrons::MSource::TGaussSmear<FIMPL>;
