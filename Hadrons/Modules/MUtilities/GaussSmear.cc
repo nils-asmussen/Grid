da395c7fd2bda2907cee741275d723233ff687c2
Nils Asmussen <n.asmussen@soton.ac.uk>
2019-05-20 17:26:22 +0100

    delete some modules

--- Hadrons/Modules/MUtilities/GaussSmear.cc
+++
@@ -1,7 +0,0 @@
-#include <Hadrons/Modules/MUtilities/GaussSmear.hpp>
-
-using namespace Grid;
-using namespace Hadrons;
-using namespace MUtilities;
-
-template class Grid::Hadrons::MUtilities::TGaussSmear<FIMPL>;
--- Hadrons/Modules/MUtilities/JacobiSmear.cc
+++
@@ -1,7 +0,0 @@
-#include <Hadrons/Modules/MUtilities/JacobiSmear.hpp>
-
-using namespace Grid;
-using namespace Hadrons;
-using namespace MUtilities;
-
-template class Grid::Hadrons::MUtilities::TJacobiSmear<FIMPL>;
